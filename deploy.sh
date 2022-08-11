f9b80ad878c7b1e36b3555e42782410fbe99ef55
“331296331” <331296441@qq.com>
2022-08-11 22:57:07 +0800

    first

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://331296441.github.io
+git push -f git@github.com:331296441/331296441.github.io.git master
+
+
+cd -
